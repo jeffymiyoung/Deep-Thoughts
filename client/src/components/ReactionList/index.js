219b0fcb32f8f6dd14ed213bef8dd23c6fdc1c39
Jeffrey Young <jeffreymiyoung@aol.com>
2022-08-05 12:05:57 -0400

    added minor routes and ReactionList

---
+++ client/src/components/ReactionList/index.js
@@ -0,0 +1,13 @@
+// Imports
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// ReactionList JSX
+const ReactionList = ({ reactions }) => {
+    // return (
+
+    // );
+};
+
+// Export for External
+export default ReactionList;
