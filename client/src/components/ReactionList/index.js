6c8aeda1f48b84c3895535bf65ec881e6b0a9665
Jeffrey Young <jeffreymiyoung@aol.com>
2022-08-06 09:02:22 -0400

    added routes and ReactionList/FriendList

--- client/src/components/ReactionList/index.js
+++ client/src/components/ReactionList/index.js
@@ -4,9 +4,24 @@ import { Link } from 'react-router-dom';
 
 // ReactionList JSX
 const ReactionList = ({ reactions }) => {
-    // return (
-
-    // );
+    return (
+        <div className="card mb-3">
+            <div className="card-header">
+                <span className="text-light">reactions</span>
+            </div>
+            <div className="card-body">
+                {reactions &&
+                    reactions.map(reaction => (
+                        <p className="pill mb-3" key={reaction._id}>
+                            {reaction.reactionBody} {'// '}
+                            <Link to={`/profile/${reaction.username}`} style={{ fontWeight: 700 }}>
+                                {reaction.username} on {reaction.createdAt}
+                            </Link>
+                       </p>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 // Export for External
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,9 @@
 // Imports
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { userQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -28,6 +29,7 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
